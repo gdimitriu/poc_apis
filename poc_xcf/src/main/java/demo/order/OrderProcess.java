cd8c490d3a17b01259cdcad028d1c4f6a2ac64b7
gdimitriu <gabriel.dimitriu@yahoo.com>
2020-06-16 20:43:32 +0300

    new version

---
+++ poc_xcf/src/main/java/demo/order/OrderProcess.java
@@ -0,0 +1,10 @@
+package demo.order;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface OrderProcess {
+    @WebMethod
+    String processOrder(Order order);
+}
